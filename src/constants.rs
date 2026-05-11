ccf68e01f346dd788fe961a56b7e33ad84d24c33
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-05-11 14:26:34 +0200

    feat: add nix flake compat, update docs

--- src/constants.rs
+++ src/constants.rs
@@ -6,5 +6,5 @@ pub static CPU_CORES_LOGICAL: OnceLock<f32> = OnceLock::new();
 // in order: normal process, arch binary, canary client, vesktop (vencord)
 pub const DISCORD_PROCESS_NAMES: [&str; 4] = ["discord", "discord-bin", "discordcanary", "vesktop"];
 
-pub const NOTIF_TITLE: &str = "Discord Memory Leak";
-pub const NOTIF_BODY: &str = "Your Discord app is experiencing a memory leak. If you are currently streaming, try restarting it";
+pub const NOTIF_TITLE: &str = "dmn";
+pub const NOTIF_BODY: &str = "Your Discord app is experiencing a resource leak. If you are currently streaming, try restarting it";
